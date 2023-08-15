2c804052f03c3509264bc0522bc7eb86f4ed9d9d
loganrehberg <loganrehberg7@gmail.com>
2023-08-14 23:09:29 -0400

    debugged index.js and added test folder

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Required Files
-const inquirer = require('inquirer');
+const inquirer = require("inquirer");
 const shapes = require("./lib/shapes.js");
 const fs = require("fs");
 
@@ -10,19 +10,18 @@ const questions = [
     type: "input",
     name: "text",
     message: "Enter message here (3 character limit):",
-    
   },
   //Shape
   {
     type: "list",
     name: "shape",
     message: "choose your shape",
-    choice: ["Circle", "Square", "Triangle"],
+    choices: ["Circle", "Square", "Triangle"],
   },
   //Text color
   {
     type: "input",
-    color: "textColor",
+    name: "textColor",
     message: "Enter message here (name or hex)",
   },
   //Shape color
@@ -33,28 +32,34 @@ const questions = [
   },
 ];
 
-//Function to generate the SVG logo
-function generateSVG(text, textColor, shape, shapeColor) {
-  let svgContent = '';
+// Function to generate the SVG logo
+// function generateSVG(text, textColor, shape, shapeColor) {
+//   let svgContent = "";
 
-  switch (shape.toLowerCase()) {
-    case 'circle':
-      svgContent = `<circle cx="150" cy="100" r="50" fill="${shapeColor}" />`;
-      break;
-    case 'triangle':
-      svgContent = `<polygon points="150,50 100,150 200,150" fill="${shapeColor}" />`;
-      break;
-    case 'square':
-      svgContent = `<rect x="100" y="50" width="100" height="100" fill="${shapeColor}" />`;
-      break;
-    default:
-      throw new Error('Invalid shape choice.');
-  }
+//   switch (shape.toLowerCase()) {
+//     case "circle":
+//       svgContent = `<circle cx="150" cy="100" r="50" fill="${shapeColor}" />`;
+//       break;
+//     case "triangle":
+//       svgContent = `<polygon points="150,50 100,150 200,150" fill="${shapeColor}" />`;
+//       break;
+//     case "square":
+//       svgContent = `<rect x="100" y="50" width="100" height="100" fill="${shapeColor}" />`;
+//       break;
+//     default:
+//       throw new Error("Invalid shape choice.");
+//   }
 
-  svgContent += `<text x="150" y="120" fill="${textColor}" font-family="Arial" font-size="20" text-anchor="middle">${text}</text>`;
+//   svgContent += `<text x="150" y="120" fill="${textColor}" font-family="Arial" font-size="20" text-anchor="middle">${text}</text>`;
 
-  return `<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">${svgContent}</svg>`;
-}
+//   return `<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">${svgContent}</svg>`;
+// }
+// variable to createSVG
+const generateSVG = (text, textColor, shapeElement) => {
+  return `
+  <svg xmlns="http://www.w3.org/2000/svg" width="300" height="200">${shapeElement}<text x="150" y="100" font-family="Arial" font-size="20" text-anchor="middle" fill="${textColor}">${text}</text></svg>
+  `
+};
 //Function to create SVG file
 function write(data) {
   fs.writeFile("./examples/logo.svg", data, (err) =>
@@ -67,9 +72,9 @@ function write(data) {
 function init() {
   inquirer.prompt(questions).then((answers) => {
     const { text, textColor, shape, shapeColor } = answers;
-    let shapeElement = GenerateShape.GenerateShapeElement(answers);
-    let svg = generateSVG(text, textColor, shape);
-    write(SVG);
+    let shapeElement = shapes.generateSVGshape(answers);
+    let svg = generateSVG(text, textColor, shapeElement);
+    write(svg);
   });
 }
 
